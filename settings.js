56f0c4b273dd84d13fd1121d0a721a6278af6c3c
jsiebfbfbf <ishanmadusanke897@gmail.com>
2026-05-09 18:41:46 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 
-    MONGO_URI: process.env.MONGO_URI || 'mongodb+srv://ishanccntxpr_db_user:k1gvugvb5l0ONP88@cluster0.ltwn3yv.mongodb.net/',
+    MONGO_URI: process.env.MONGO_URI || 'mongodb+srv://ishanccntxpr:k1gvugvb5l0ONP88@cluster0.ltwn3yv.mongodb.net/',
     MONGO_DB: process.env.MONGO_DB || 'ishanxkavyaxmd',
     
     PREFIX: process.env.PREFIX || '.',
