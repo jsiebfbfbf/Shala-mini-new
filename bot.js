d3b2fb328860f9d8f9652aa82c9a5da777e28213
jsiebfbfbf <ishanmadusanke897@gmail.com>
2026-05-11 21:16:14 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -28,7 +28,7 @@ const {
   jidNormalizedUser,
   downloadContentFromMessage,
   DisconnectReason
-} = require('dct-dev-private-baileys');
+} = require('dct-dula-baileys');
 
 const initNumberSystem = require('./lib/numberSystem'); 
 const numberSystems = new Map();  
